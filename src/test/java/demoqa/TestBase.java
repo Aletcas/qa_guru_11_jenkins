0dd6506c8d2855340402e5cadf0214c221c91411
Aletcas <LinaKiri@yandex.ru>
2022-03-09 19:21:22 +0300

    gait added

--- src/test/java/demoqa/TestBase.java
+++ src/test/java/demoqa/TestBase.java
@@ -19,7 +19,7 @@ public class TestBase {
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
         // для запуска на удаленном селеноиде
-      //  Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
         // конфигурация удаленного запуска
         DesiredCapabilities capabilities = new DesiredCapabilities();
